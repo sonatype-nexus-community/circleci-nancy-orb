cf936fcc584624e3b6c6bb8c84f32a03e820fe40
Dan Rollo <danrollo@gmail.com>
2020-01-31 14:01:25 -0500

    cleanup local helper script

--- inline-orb/runcircle-local-inline.sh
+++ inline-orb/runcircle-local-inline.sh
@@ -1,4 +1,5 @@
 # exit when any command fails
 set -e
-circleci config process inline-orb/config-inline.yml > inline-orb/local-config-inline.yml && circleci local execute -c inline-orb/local-config-inline.yml --job 'job-test-inline-orb'
-circleci config process inline-orb/config-inline.yml > inline-orb/local-config-inline.yml && circleci local execute -c inline-orb/local-config-inline.yml --job 'job-extract-inline-orb'
+circleci config process inline-orb/config-inline.yml > inline-orb/local-config-inline.yml
+circleci local execute -c inline-orb/local-config-inline.yml --job 'job-test-inline-orb'
+circleci local execute -c inline-orb/local-config-inline.yml --job 'job-extract-inline-orb'
