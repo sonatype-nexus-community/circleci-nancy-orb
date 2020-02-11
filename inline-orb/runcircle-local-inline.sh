ee530471801d9ffc55db552212239a9ba7d41318
Dan Rollo <danrollo@gmail.com>
2020-02-11 17:32:06 -0500

    convert to 'orb-starter-kit' layout, removing inline orb

--- inline-orb/runcircle-local-inline.sh
+++
@@ -1,5 +0,0 @@
-# exit when any command fails
-set -e
-circleci config process inline-orb/config-inline.yml > inline-orb/local-config-inline.yml
-circleci local execute -c inline-orb/local-config-inline.yml --job 'job-test-inline-orb'
-circleci local execute -c inline-orb/local-config-inline.yml --job 'job-extract-inline-orb'
